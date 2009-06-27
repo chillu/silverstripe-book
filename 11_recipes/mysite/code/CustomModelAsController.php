ca38011599777ae0506ef0407efe6b420ded0e91
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-06-27 06:41:16 +0000

    updated recipe code to match 2.3.2 and be more flexible about passed url structures (extensions and subfolders)
    git-svn-id: http://svn.silverstripe.com/projects/ssbook_wiley_src/trunk@80218 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- 11_recipes/mysite/code/CustomModelAsController.php
+++ 11_recipes/mysite/code/CustomModelAsController.php
@@ -1,22 +1,28 @@
 <?php
 class CustomModelAsController extends ModelAsController {
-	public function getNestedController() {
-		if($this->urlParams['URLSegment']) {
-			$SQL_URLSegment = Convert::raw2sql(
-				$this->urlParams['URLSegment']
-			);
-			$page = DataObject::get_one(
-				'Page',
-				"`SiteTree`.LegacyURL = '{$SQL_URLSegment}'"
-			);
-			if($page) {
-				$response = new HTTPResponse();
-				$response->redirect($page->URLSegment, 301);
-				return $response;
-			}
+	public function handleRequest($request) {
+			$this->request = $request;
+			return parent::handleRequest($request);
 		}
-		
-		return parent::getNestedController();
-	}
+		public function getNestedController() {
+			$url = $this->request->getUrl();
+			$getVars = $this->request->getVars();
+			if(isset($getVars['url'])) unset($getVars['url']);
+			if($getVars) $url .= '?' . http_build_query($getVars);
+			if($url) {
+				$SQL_URLSegment = Convert::raw2sql($url);
+				$page = DataObject::get_one(
+					'Page',
+					"`SiteTree`.LegacyURL = '{$SQL_URLSegment}'"
+				);
+				if($page) {
+					$response = new HTTPResponse();
+					$newUrl = Director::baseURL() . $page->URLSegment;
+					$response->redirect($newUrl, 301);
+					return $response;
+				}
+			}
+			return parent::getNestedController();
+		}
 }
 ?>
