9eb7293f6e609d1af448d8033ccf7cb888ac1c5b
jseide <jseide@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-07-10 02:37:32 +0000

    Updated branches
    git-svn-id: http://svn.silverstripe.com/projects/ssbook_wiley_src/trunk@81567 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- 5_development/mysite/_config.php
+++ 5_development/mysite/_config.php
@@ -3,6 +3,10 @@
 global $project;
 $project = 'mysite';
 
+global $database;
+$database = "three_5";
+
+
 // Use _ss_environment.php file for configuration
 // see http://doc.silverstripe.com/doku.php?id=environment-management
 require_once("conf/ConfigureFromEnv.php");
