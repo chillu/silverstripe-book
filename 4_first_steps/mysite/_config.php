d549e1c7f8cb8c55df9f3353214a3db8a26efca9
jseide <jseide@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-07-10 00:22:32 +0000

    Updated to branches/2.3
    git-svn-id: http://svn.silverstripe.com/projects/ssbook_wiley_src/trunk@81541 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- 4_first_steps/mysite/_config.php
+++ 4_first_steps/mysite/_config.php
@@ -4,9 +4,11 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = "";
+$database = "three_4";
 
 require_once("conf/ConfigureFromEnv.php");
+
+Security::setDefaultAdmin('admin', 'password');
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
